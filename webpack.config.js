f23790f49bc61c3fdba3b23bb6b1a38dc9d78965
Vladimir Tolstikov <wertyxa@bk.ru>
2017-12-03 16:00:16 +0500

    removed use cache from ts loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     loaders: [
       {
         test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader?useCache=true',
+        loader: 'awesome-typescript-loader',
       },
     ],
   },
